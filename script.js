006613bece4ad54d1e526770304cab197258e529
yash more1224 <yashsmore7499@gmail.com>
2025-06-20 23:42:14 +0530

    js extracts title and note from index.html:

---
+++ script.js
@@ -0,0 +1,9 @@
+function save_notes(){
+    // runs when save button is clicked
+    const title = document.getElementById("note_title").value
+    const note = document.getElementById("ipbox").value
+
+    console.log(title)
+    console.log("sth is wrng")
+    console.log(note)
+} 
