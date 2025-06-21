b44896a5333f98f41184554f98acd6a51770171f
yash more1224 <yashsmore7499@gmail.com>
2025-06-21 20:30:10 +0530

    post methods worksgs, lots of errors i did in the procedure tho :(

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pydantic import BaseModel
-from fastapi.cors.middleware import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.add_Middleware(
+app.add_middleware(
     CORSMiddleware, 
-    add_methods = ["*"],
-    add_headers = ["*"],
-    add_origins = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+    allow_origins = ["*"],
 ); 
 
-client = MongoClient['mongodb+srv://yashsmore7499:<db_password>@cluster0.hom9mcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0']
+client = MongoClient('mongodb+srv://yashsmore7499:DaqunbmFErcXHS10@cluster0.hom9mcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client['takenotes']
 collection = db['users']
 
@@ -26,7 +26,7 @@ async def root():
 
 @app.post("/save-notes")
 async def save_notes(data: resModel):
-    collection.insertOne({"title":data.title, "content":data.content})
+    collection.insert_one({"title":data.title, "content":data.content})
     return {"message": "note saved sucessfully!"}
 
 @app.get("/get-notes")
--- script.js
+++ script.js
@@ -3,12 +3,12 @@ async function save_notes(){
     const title = document.getElementById("note_title").value
     const note = document.getElementById("ipbox").value
 
-    const res = await fetch('/save-notes', {
-        method: 'post', 
-        headers: {'Contet-Type':"application/json"}, 
+    const res = await fetch("http://127.0.0.1:8000/save-notes", { // returns a promise that resolves to a "response" obj
+        method: "post", 
+        headers: {"Content-Type":"application/json"}, 
         body: JSON.stringify({"title": title, "content": note}, null, 2)        
     });
 
-    const response = await res.json()
-    console.log(response.json())
+    const response = await res.json() // returns a "response" obj that provided json data as body
+    console.log(response.message)
 }
